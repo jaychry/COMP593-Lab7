d7290b264a5806ce1555ac3f42dea204dff6a4d6
jaychry <jaychaudhary0200@gmail.com>
2025-03-10 18:40:01 -0400

    update script
    create table function

--- create_db.py
+++ create_db.py
@@ -33,3 +33,30 @@ def create_movies_table():
     con.commit()
     con.close()
     return
+
+def populate_movies_table():
+    """Populates the movies table with data from jamesbond.csv"""
+    con = sqlite3.connect('bond_movies.db')
+    cur = con.cursor()
+    add_movie_query = """INSERT INTO movies (
+                            Year,
+                            Movie,
+                            Bond,
+                            Avg_User_IMDB
+    )
+    VALUES (?, ?, ?, ?)"""
+
+    if not os.path.exists('jamesbond.csv'):
+        print("Error: jamesbond.csv not found.")
+        return
+    
+    bond_df = pd.read_csv('jamesbond.csv')
+
+    for row in bond_df.itertuples(index=False):
+        cur.execute(add_movie_query, (row.Year, row.Movie, row.Bond, row.Avg_User_IMDB))
+    con.commit()
+    con.close()
+    return
+
+if __name__ == '__main__':
+    main()
